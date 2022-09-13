3ff3f8b98c536a56b1ca38ca5f4edce0d2834e2f
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2022-09-13 07:07:04 -0300

    React course

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Texto from './components/Texto';
+
+function App(){
+    return (
+        <>
+            <h1>Comentários</h1>
+            <Texto></Texto>
+        </>
+    )
+}
+
+export default App
---
+++ src/components/Texto.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Texto.css';
+
+function Texto(){
+    return (
+        <section className="coment">
+            <p className="title">Título </p>
+            <p className="text">Texto do comentário</p>
+            <p className="author">Autor: Juliana</p>
+        </section>
+    )
+}
+
+export default Texto
